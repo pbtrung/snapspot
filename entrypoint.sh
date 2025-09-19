199cbf66963b28351d50818aa5efb12268fd4438
Trung Pham <trungpb2@vmogroup.com>
2025-09-19 17:50:29 +0700

    Fix autoplay

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,6 @@ mkdir -p /music/oauth
 librespot --cache /music/oauth --enable-oauth \
     --backend pipe --enable-volume-normalisation \
     --device /tmp/snapfifo --bitrate 320 \
-    --autoplay --initial-volume 100 \
+    --autoplay on --initial-volume 100 \
     --cache-size-limit 100M --name music \
     --normalisation-gain-type track
