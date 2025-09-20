a037feba117f42a70d1c646ea063ef1226734f1a
Trung Pham <pbtrung@outlook.com>
2025-09-20 07:16:57 +0700

    Refactor

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,14 @@
 snapserver --config "$1" &
 
 mkdir -p /music/oauth
-librespot --cache /music/oauth --enable-oauth \
-    --backend pipe --enable-volume-normalisation \
-    --device /tmp/snapfifo --bitrate 320 \
-    --autoplay on --initial-volume 100 \
-    --cache-size-limit 100M --name music \
+librespot --name music \
+    --cache /music/oauth \
+    --enable-oauth \
+    --backend pipe \
+    --device /tmp/snapfifo \
+    --bitrate 320 \
+    --autoplay on \
+    --initial-volume 100 \
+    --cache-size-limit 100M \
+    --enable-volume-normalisation \
     --normalisation-gain-type track
